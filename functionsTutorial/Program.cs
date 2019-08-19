3cbe8a95ae6b5170b746c1e5966d47bc05f4481d
Hunter Trahan <s198054@students.aie.edu>
2019-08-19 17:26:34 -0500

    Add project files.

---
+++ functionsTutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionsTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Makes new object from Game Template
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
